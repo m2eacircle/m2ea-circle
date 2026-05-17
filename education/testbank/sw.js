73322deab8306efe6dbf62ad2d138f3f361dee47
m2eacircle <donguk.chai@gmail.com>
2026-05-16 23:28:18 -0400

    Add files via upload

---
+++ education/testbank/sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'dental-testbank-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.svg',
+  '/favicon-16x16.svg',
+  '/favicon-32x32.svg',
+  '/apple-touch-icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
